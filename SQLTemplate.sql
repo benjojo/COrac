4616c8d52cc77726c5b9ca6cce6d1bf3153de941
Ben Cox <Ben@Benjojo.co.uk>
2013-03-09 15:55:45 +0000

    New SQL Table

--- SQLTemplate.sql
+++ SQLTemplate.sql
@@ -1,31 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 3.5.6
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 18, 2013 at 12:13 PM
--- Server version: 5.1.66-0+squeeze1
--- PHP Version: 5.3.3-7+squeeze14
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `notorac2`
---
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               5.5.24-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2013-03-09 15:54:51
 -- --------------------------------------------------------
 
---
--- Table structure for table `Problems`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
+-- Dumping structure for table notorac.attempts
+CREATE TABLE IF NOT EXISTS `Attempts` (
+  `AttemptID` int(10) NOT NULL AUTO_INCREMENT,
+  `ProblemID` int(10) NOT NULL,
+  `Code` text NOT NULL,
+  `Output` text NOT NULL,
+  `CompilerOutput` text NOT NULL,
+  `ExecTime` float NOT NULL,
+  PRIMARY KEY (`AttemptID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table notorac.problems
 CREATE TABLE IF NOT EXISTS `Problems` (
   `ProblemID` int(11) NOT NULL AUTO_INCREMENT,
   `ClassID` varchar(8) NOT NULL,
@@ -35,25 +34,23 @@ CREATE TABLE IF NOT EXISTS `Problems` (
   `Solution` text NOT NULL,
   `ValueGenerationM` varchar(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (`ProblemID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `Users`
---
 
+-- Dumping structure for table notorac.users
 CREATE TABLE IF NOT EXISTS `Users` (
   `UserID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(32) NOT NULL,
   `ClassID` varchar(8) NOT NULL,
-  `Password` varchar(64) NOT NULL,
+  `Password` varchar(92) NOT NULL,
   `Email` varchar(128) NOT NULL,
   `DateRegistered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `GlobalScore` int(11) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- Data exporting was unselected.
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
